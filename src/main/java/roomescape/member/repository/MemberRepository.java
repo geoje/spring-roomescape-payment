6e66d3e91c52a21325c8f32a580fa7927973e69a
Gyeongho Yang <chchch1213@naver.com>
2024-07-10 14:56:57 +0900

    feat: Add findByRole function on MemberRepository

--- src/main/java/roomescape/member/repository/MemberRepository.java
+++ src/main/java/roomescape/member/repository/MemberRepository.java
@@ -2,10 +2,13 @@ package roomescape.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import roomescape.member.domain.Member;
+import roomescape.member.domain.Role;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
+
+    Optional<Member> findByRole(Role role);
 }
